bfa355bcac41ba3cb99f8f1fc0bfea4fafb5d256
Fidel Nyame <nyamefidel7@gmail.com>
2025-10-29 15:39:45 +0000

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <p className='text-[4rem]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque sint eveniet laudantium incidunt fuga accusamus tempore minus, culpa iure quas totam veritatis temporibus minima quos iste asperiores qui quibusdam quis!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates minus ullam rem dolore omnis provident saepe temporibus quaerat distinctio eos sed cupiditate, vel soluta doloremque commodi error exercitationem? Vero, suscipit.</p>
+    </div>
+  )
+}
+
+export default Home
