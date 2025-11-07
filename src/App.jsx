2a411f89ff5c9776df86c0043c1fae80451d4c3c
Fidel Nyame <nyamefidel7@gmail.com>
2025-11-07 10:19:16 +0000

    card assignment

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Card from './components/cardUI'
+
+const App = () => {
+  return (
+    <Card />
+  )
+}
+
+export default App
---
+++ src/components/cardUI.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Card = () => {
+  return (
+    <>
+    <div className='w-[20rem] h-[25rem] bg-black/40 rounded-xl mt-20 place-self-center'>
+    <img src="/src/assets/hulvey.png" alt="" className='w-full h-full object-cover rounded-xl'/>
+    </div>
+    </>
+  )
+}
+
+export default Card
