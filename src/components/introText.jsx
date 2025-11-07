6e92882b4e32f101a08550e0a81954520871025a
Fidel Nyame <nyamefidel7@gmail.com>
2025-11-07 10:57:47 +0000

    card ui

--- src/components/introText.jsx
+++ src/components/introText.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+
 const Intro = () => {
+  const color = "#F54B28";
   return (
     <div className='flex flex-col space-y-5'>
-       <div className='text-[2.3rem] place-self-center text-white'>Find your sound. <span className='text-white/50 italic text-white'>Find your People</span></div> 
+       <div className='text-[2.3rem] place-self-center text-white'>Find your sound. <span className='text-[#00F5D4] italic ' >Find your People</span></div> 
 
        <div className='text-[1.5rem] text-[white]/80 place-self-center'>
         Find your perfect match through the music that moves you.
